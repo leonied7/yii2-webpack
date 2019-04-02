433c8b02f220b539f287a3cfb741dd08c0859a42
Denis <kdnn@mail.ru>
2019-04-02 16:01:34 +0700

    1

--- helpers/entry.js
+++ helpers/entry.js
@@ -5,16 +5,19 @@ const path = require('path')
 module.exports = {
   /**
    * @param {object} entry
+   * @param assetDir
+   * @param options
    * @return {object[]}
    */
-  generateHtmlPlugins: (entry) => {
+  generateHtmlPlugins: (entry, {assetDir, ...options}) => {
     return Object.keys(entry).reduce((result, key) => {
       result.push(new HtmlWebpackPlugin({
         inject: false,
         template: path.resolve(__dirname, '../index.ejs'),
         chunks: [],
         entry: key,
-        filename: path.resolve(process.cwd(), 'assets/' + _.upperFirst(_.camelCase(key)) + 'Asset.php'),
+        additionOptions: options,
+        filename: path.resolve(process.cwd(), assetDir + '/' + _.upperFirst(_.camelCase(key)) + 'Asset.php'),
         alwaysWriteToDisk: true
       }))
       return result
--- webpack.common.js
+++ webpack.common.js
@@ -3,10 +3,11 @@ const entryHelper = require('./helpers/entry')
 const merge = require('webpack-merge')
 const configLoader = require('./helpers/config')
 const common = configLoader('./webpack.common.js')
+const settings = require('./settings')
 
 module.exports = merge.smart({
   plugins: [
-    ...entryHelper.generateHtmlPlugins(common.entry),
+    ...entryHelper.generateHtmlPlugins(common.entry, settings),
     new ChunksFromEntryPlugin()
   ]
 }, common)
