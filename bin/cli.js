3e055b0142fd6c2dc2d279b1699b01b0d000fb41
Denis <kdnn@mail.ru>
2019-04-02 15:35:44 +0700

    1

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
-
-const inquirer  = require('../lib/inquirer');
 const defaultValues = require('../lib/defaultValues')
 const fs = require('fs')
-const [,, ...args] = process.argv
+const [, , ...args] = process.argv
 
 const run = async () => {
   let credentials = {}
-  if(args.includes('--default-config')) {
+  if (args.includes('--default-config')) {
     credentials = defaultValues
   } else {
-    credentials = await inquirer.askQuestions();
+    const inquirer = require('../lib/inquirer')
+    credentials = await inquirer.askQuestions()
   }
   fs.writeFileSync('settings.json', JSON.stringify(credentials))
 }
 
-run();
+run()
