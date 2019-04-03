7c8bb973bb6c9e81c10214a585b474f03491404c
Denis <kdnn@mail.ru>
2019-04-03 15:13:21 +0700

    если не существует файл настроек, то криво работает

--- webpack.common.js
+++ webpack.common.js
@@ -3,11 +3,12 @@ const entryHelper = require('./helpers/entry')
 const merge = require('webpack-merge')
 const configLoader = require('./helpers/config')
 const common = configLoader('./webpack.common.js')
-const settings = configLoader('./yii2-webpack-settings.json') || require('./lib/defaultValues')
+const settings = configLoader('./yii2-webpack-settings.json')
+const _ = require('lodash')
 
 module.exports = merge.smart({
   plugins: [
-    ...entryHelper.generateHtmlPlugins(common.entry, settings),
+    ...entryHelper.generateHtmlPlugins(common.entry, _.extend(require('./lib/defaultValues'), settings)),
     new ChunksFromEntryPlugin()
   ]
 }, common)
