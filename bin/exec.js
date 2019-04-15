67728b68be6612447a1a5d392336c9e177e776b9
Denis <kdnn@mail.ru>
2019-04-15 13:11:03 +0700

    убрана возможность передавать конфиг webpack, формируется в зависимости от параметра yii2-webpack-config

--- bin/exec.js
+++ bin/exec.js
@@ -12,14 +12,8 @@ yargs.options({
   'yii2-webpack-command': {
     type: 'string',
     describe: 'Webpack command',
-    defaultDescription: "webpack or webpack-dev-server",
+    defaultDescription: 'webpack or webpack-dev-server',
     default: yargs.argv['yii2-webpack-config'] === 'dev' ? 'webpack-dev-server' : 'webpack',
-    demandOption: true
-  },
-  config: {
-    type: 'string',
-    describe: 'Path to the config file',
-    default: path.relative(process.cwd(), `${path.dirname(__dirname)}/webpack.${yargs.argv['yii2-webpack-config']}.js`),
     demandOption: true
   }
 })
@@ -29,11 +23,14 @@ process.argv = _.reduce(yargs.argv, (result, value, key) => {
     return result
   }
   result.push(`--${key}`)
-  if(value !== true && value) {
+  if (value !== true && value) {
     result.push(value)
   }
   return result
 }, process.argv.slice(0, 2))
+
+process.argv.push('--config')
+process.argv.push(path.relative(process.cwd(), `${path.dirname(__dirname)}/webpack.${yargs.argv['yii2-webpack-config']}.js`))
 
 const command = yargs.argv['yii2-webpack-command']
 const pkgPath = require.resolve(`${command}/package.json`)
